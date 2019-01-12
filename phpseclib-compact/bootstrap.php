f5892d23984b621ea1b89ffe23bade019aede90c
Mathis Klooß <mathis@klooss.net>
2019-01-12 15:54:36 +0100

    compatible with magento 1.9.4.0

--- phpseclib-compact/bootstrap.php
+++ phpseclib-compact/bootstrap.php
@@ -1,31 +1,51 @@
 <?php
 
-if (!defined('PHPSECLIB_VERSION'))
-{
-    define('PHPSECLIB_VERSION', '2.0.11');
+$dir = null;
+if (!defined('MAGENTO_PHPSECLIB')) {
+    if (version_compare(Mage::getVersion(), '1.9.4.0') >= 0)
+    {
+        define('MAGENTO_PHPSECLIB', true);
+    } else {
+        define('MAGENTO_PHPSECLIB', false);
+    }
 }
 
-require_once 'phpseclib-'.PHPSECLIB_VERSION.'/Math/BigInteger.php';
-require_once 'phpseclib-'.PHPSECLIB_VERSION.'/Crypt/Base.php';
-require_once 'phpseclib-'.PHPSECLIB_VERSION.'/Crypt/DES.php';
-require_once 'phpseclib-'.PHPSECLIB_VERSION.'/Crypt/RC2.php';
-require_once 'phpseclib-'.PHPSECLIB_VERSION.'/Crypt/TripleDES.php';
-require_once 'phpseclib-'.PHPSECLIB_VERSION.'/Crypt/Blowfish.php';
-require_once 'phpseclib-'.PHPSECLIB_VERSION.'/Crypt/Random.php';
-require_once 'phpseclib-'.PHPSECLIB_VERSION.'/Crypt/Hash.php';
-require_once 'phpseclib-'.PHPSECLIB_VERSION.'/Crypt/Rijndael.php';
-require_once 'phpseclib-'.PHPSECLIB_VERSION.'/Crypt/RC4.php';
-require_once 'phpseclib-'.PHPSECLIB_VERSION.'/Crypt/RSA.php';
-require_once 'phpseclib-'.PHPSECLIB_VERSION.'/Crypt/Twofish.php';
-require_once 'phpseclib-'.PHPSECLIB_VERSION.'/Crypt/AES.php';
-require_once 'phpseclib-'.PHPSECLIB_VERSION.'/File/X509.php';
-require_once 'phpseclib-'.PHPSECLIB_VERSION.'/File/ASN1/Element.php';
-require_once 'phpseclib-'.PHPSECLIB_VERSION.'/File/ANSI.php';
-require_once 'phpseclib-'.PHPSECLIB_VERSION.'/File/ASN1.php';
-require_once 'phpseclib-'.PHPSECLIB_VERSION.'/System/SSH/Agent/Identity.php';
-require_once 'phpseclib-'.PHPSECLIB_VERSION.'/System/SSH/Agent.php';
-require_once 'phpseclib-'.PHPSECLIB_VERSION.'/Net/SCP.php';
-require_once 'phpseclib-'.PHPSECLIB_VERSION.'/Net/SSH1.php';
-require_once 'phpseclib-'.PHPSECLIB_VERSION.'/Net/SSH2.php';
-require_once 'phpseclib-'.PHPSECLIB_VERSION.'/Net/SFTP.php';
-require_once 'phpseclib-'.PHPSECLIB_VERSION.'/Net/SFTP/Stream.php';
+if (!MAGENTO_PHPSECLIB) {
+    if (!defined('PHPSECLIB_VERSION'))
+    {
+        define('PHPSECLIB_VERSION', '2.0.13');
+        $dir = 'phpseclib-' . PHPSECLIB_VERSION;
+    }
+} else {
+    if (!defined('BP'))
+    {
+        throw new Exception('BP is not set');
+    }
+    $dir = BP . DS . 'lib' . DS . 'phpseclib';
+    require_once $dir . '/bootstrap.php';
+}
+
+require_once $dir . '/Math/BigInteger.php';
+require_once $dir . '/Crypt/Base.php';
+require_once $dir . '/Crypt/DES.php';
+require_once $dir . '/Crypt/RC2.php';
+require_once $dir . '/Crypt/TripleDES.php';
+require_once $dir . '/Crypt/Blowfish.php';
+require_once $dir . '/Crypt/Random.php';
+require_once $dir . '/Crypt/Hash.php';
+require_once $dir . '/Crypt/Rijndael.php';
+require_once $dir . '/Crypt/RC4.php';
+require_once $dir . '/Crypt/RSA.php';
+require_once $dir . '/Crypt/Twofish.php';
+require_once $dir . '/Crypt/AES.php';
+require_once $dir . '/File/X509.php';
+require_once $dir . '/File/ASN1/Element.php';
+require_once $dir . '/File/ANSI.php';
+require_once $dir . '/File/ASN1.php';
+require_once $dir . '/System/SSH/Agent/Identity.php';
+require_once $dir . '/System/SSH/Agent.php';
+require_once $dir . '/Net/SCP.php';
+require_once $dir . '/Net/SSH1.php';
+require_once $dir . '/Net/SSH2.php';
+require_once $dir . '/Net/SFTP.php';
+require_once $dir . '/Net/SFTP/Stream.php';
