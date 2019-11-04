12435d43b4da65267bef03235e6fe9203433e099
Mathis Klooß <mathis@mage-profis.de>
2019-11-04 10:52:17 +0100

    :(

--- phpseclib-compact/Crypt/Base.php
+++ phpseclib-compact/Crypt/Base.php
@@ -2,7 +2,11 @@
 
 require_once BP.DS.'app'.DS.'code'.DS.'community'.DS.'phpseclib'.DS.'Crypt'.DS.''. basename(__FILE__);
 
-class Crypt_Base
-extends \phpseclib\Crypt\Base
+// check if this class is already loaded, some modules load this file wrong :(
+if (!class_exists('Crypt_Base', false))
 {
+    class Crypt_Base
+    extends \phpseclib\Crypt\Base
+    {
+    }
 }
