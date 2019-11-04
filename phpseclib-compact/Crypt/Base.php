05958d44980cb7f5b26faf78d429a69c0c9ce842
Mathis Klooß <mathis@mage-profis.de>
2019-11-04 10:50:12 +0100

    removed abstract definition

--- phpseclib-compact/Crypt/Base.php
+++ phpseclib-compact/Crypt/Base.php
@@ -2,7 +2,7 @@
 
 require_once BP.DS.'app'.DS.'code'.DS.'community'.DS.'phpseclib'.DS.'Crypt'.DS.''. basename(__FILE__);
 
-abstract class Crypt_Base
+class Crypt_Base
 extends \phpseclib\Crypt\Base
 {
 }
