45173679727da734f3e53ac6741050b4b5ed0b9e
Mathis Klooß <mathis@mage-profis.de>
2019-11-21 14:38:01 +0100

    added abstract classes

--- phpseclib-compact/Crypt/Base.php
+++ phpseclib-compact/Crypt/Base.php
@@ -8,5 +8,17 @@ if (!class_exists('Crypt_Base', false))
     class Crypt_Base
     extends \phpseclib\Crypt\Base
     {
+        public function _decryptBlock($in) {
+            user_error((version_compare(PHP_VERSION, '5.0.0', '>=')  ? __METHOD__ : __FUNCTION__)  . '() must extend by class ' . get_class($this), E_USER_ERROR);
+        }
+
+        public function _encryptBlock($in) {
+            user_error((version_compare(PHP_VERSION, '5.0.0', '>=')  ? __METHOD__ : __FUNCTION__)  . '() must extend by class ' . get_class($this), E_USER_ERROR);
+        }
+
+        public function _setupKey() {
+            user_error((version_compare(PHP_VERSION, '5.0.0', '>=')  ? __METHOD__ : __FUNCTION__)  . '() must extend by class ' . get_class($this), E_USER_ERROR);
+        }
+
     }
 }
